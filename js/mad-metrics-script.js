9545c122c61b3dc46fc75b5de2db545f19ad3a03
Robert Camp <robert.camp@outlook.com>
2013-09-23 12:38:01 -0600

    got that function working

--- js/mad-metrics-script.js
+++ js/mad-metrics-script.js
@@ -1,3 +1,4 @@
+/////////----section elements intead of divs?
 $(document).ready(function(){
 
 // Mouse tracking heatmap?(has log function on page)
@@ -9,10 +10,8 @@ $(document).ready(function(){
 // });
 //
 
-
 //
-// measuring % page viewed
-
+//Begin -  Measuring % Page View & Distance Scrolled
 var scrollTop = 0;
 var scrollTopLast = 0;
 var scrollDiff = 0; 
@@ -26,7 +25,6 @@ var maxScroll = 0;
 var addScroll = 0;
 var totalScroll = 0;
 
-
 $(window).scroll(function(){
 		 scrollTop = $(window).scrollTop(); // distance scrolled from top
 		 docHeight = $(document).height();
@@ -35,7 +33,6 @@ $(window).scroll(function(){
 		 scrollPercent = ((scrollTop) / (docHeight - winHeight) * 100);
 		 scrollPercentRounded = (Math.round(scrollPercent));
 		 pageViewPercent =  winViewPercent + (scrollTop / docHeight)
-
 
 			if(scrollPercent>maxScroll){
 				maxScroll = scrollPercent
@@ -59,31 +56,20 @@ $(window).scroll(function(){
 		// console.log('scrollDiff', scrollDiff)
 	});
 
-
-
+//End - Measuring % Page View & Distance Scrolled
 //
-// Time until sign up
+//
+// Begin - Time until Sign Up
 var startTimer = new Date().getTime();
-
-console.log('startTimer', startTimer)
-
-
 var timeDiff;
-
 var signTimer;
 
 $('#signUpButton').on('click', function(){
 	 signTimer = new Date().getTime();
-
-	 // console.log('signTimer', signTimer)
-
 	 timeDiff = (signTimer - startTimer)/1000
-
-	 // console.log('timeDiffinside', timeDiff)
 });
-
-// console.log('timeDiffoutside', timeDiff)
-
+//End - Time until Sign Up
+//
 
 //Lightbox with metrics - making it appear/disappear
 var metricTimer;
@@ -111,45 +97,73 @@ var metricTimer;
 var elementTimeEnter = 0;
 var elementTimeLeave = 0;
 var elementTimeDiff = 0;
-var elementNewTimeDiffOne = 0;
+var elementNewTimeDiff = 0;
 var elementNewTimeDiffTwo = 0;
 var elementNewTimeDiffThree = 0;
 
+
+var timeObj = {}
+
 var timeArray = []
+//Old mouseTime
+// var mouseTime = function(section){
+// 		timeObj[section] = 0
+// 		elementTimeLeave = new Date().getTime();
+// 		elementTimeDiff = Math.round((elementTimeLeave - elementTimeEnter)/1000);
+// 		elementNewTimeDiff += elementTimeDiff;
+// 		timeObj[section] = timeObj[section] + elementNewTimeDiff;
+// 		console.log(timeObj)
+// 	}
+
+var mouseTime = function(section){
+
+	if (isNaN(timeObj[section]) === true){
+		timeObj[section] = 0
+
+		elementTimeLeave = new Date().getTime();
+
+		elementTimeDiff = Math.round((elementTimeLeave - elementTimeEnter)/1000);
+
+		timeObj[section]+= elementTimeDiff;
+		
+	}
+	else{
+		elementTimeLeave = new Date().getTime();
+
+		elementTimeDiff = Math.round((elementTimeLeave - elementTimeEnter)/1000);
+
+		timeObj[section]+= elementTimeDiff;
+
+	}
+
+}
+
+
+console.log('outside', timeObj)
 
 $(".divSection1").on('mouseenter', function (){
 		elementTimeEnter = new Date().getTime();
 	}).on('mouseleave', function(){
-		elementTimeLeave = new Date().getTime();
-		elementTimeDiffOne = Math.round((elementTimeLeave - elementTimeEnter)/1000);
-		elementNewTimeDiffOne += elementTimeDiffOne;
-		timeArray.splice(0, 1, elementNewTimeDiffOne); // do we need array?
-		console.log(timeArray)
-		console.log(elementNewTimeDiffOne);
-		$("#section1").text("Section 1 Time: " + (elementNewTimeDiffOne) + " seconds");
-		//return elementTimeDiff;
+		mouseTime("section1");
+		$("#section1").text("Section 1 Time: " + timeObj.section1+ " seconds");
+		timeArray.splice(0, 1, elementNewTimeDiff); // do we need array?
 	});
+		
 
 $(".divSection2").on('mouseenter', function (){
 		elementTimeEnter = new Date().getTime();
 	}).on('mouseleave', function(){
-		elementTimeLeave = new Date().getTime();
-		elementTimeDiffTwo = Math.round((elementTimeLeave - elementTimeEnter)/1000);
-		elementNewTimeDiffTwo += elementTimeDiffTwo;
-		console.log(elementNewTimeDiffTwo);
-		$("#section2").text("Section 2 Time: " + (elementNewTimeDiffTwo) + " seconds");
-		//return elementTimeDiff;
+		mouseTime("section2");
+		$("#section2").text("Section 2 Time: " + timeObj.section2 + " seconds");
+		timeArray.splice(1, 1, elementNewTimeDiff); // do we need array?
 	});
 
 $(".bigDiv").on('mouseenter', function (){
 		elementTimeEnter = new Date().getTime();
 	}).on('mouseleave', function(){
-		elementTimeLeave = new Date().getTime();
-		elementTimeDiffThree = Math.round((elementTimeLeave - elementTimeEnter)/1000);
-		elementNewTimeDiffThree += elementTimeDiffThree;
-		console.log(elementNewTimeDiffThree);
-		$("#section3").text("PeachPuff Time: " + (elementNewTimeDiffThree) + " seconds");
-		//return elementTimeDiff;
+		mouseTime("section3");
+		$("#section3").text("PeachPuff Time: " + timeObj.section3 + " seconds");
+		timeArray.splice(2, 1, elementNewTimeDiff); // do we need array?
 	});
 
 
